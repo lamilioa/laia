c9b32ae5f0edd292c2170ffa1e1e3f4e70cd2824
m5ka <m5ka+github@posteo.de>
2024-04-10 09:24:48 +0100

    💬 amend dictionary javascript

--- html/assets/js/oioa.js
+++ html/assets/js/oioa.js
@@ -15,7 +15,7 @@ class OioaLioa {
             exampleLl: false
         };
 
-        this.words = {};
+        this.words = [];
         this.subset = [];
         this.subsetLength = 0;
 
@@ -37,7 +37,7 @@ class OioaLioa {
                 return res.json();
             })
             .then((res) => {
-                this.words = res.body;
+                this.words = res;
                 this.update();
                 this.status.innerHTML = "";
             })
@@ -60,42 +60,42 @@ class OioaLioa {
     update() {
         if (this.query.length > 0) {
             this.query = this.query.toLowerCase();
-            this.subset = Object.keys(this.words)
-                .reduce((obj, key) => {
+            this.subset = this.words
+                .reduce((obj, entry) => {
                     const wholeExp = new RegExp(`(^|\\W)${this.query}($|\\W)`);
                     let match = false;
                     let whole = false;
 
-                    let definition = this.words[key].definition.toLowerCase();
+                    let definition = entry.definition.toLowerCase();
 
                     if (this.searchBy.head) {
-                        if (key == this.query) {
+                        if (entry.head === this.query) {
                             match = true;
                             whole = true;
-                        } else if (key.includes(this.query)) {
+                        } else if (entry.head.includes(this.query)) {
                             match = true;
                         }
                     }
                     if (this.searchBy.definition) {
-                        if (wholeExp.test(definition)) {
+                        if (wholeExp.test(entry.definition)) {
                             match = true;
                             whole = true;
-                        } else if (definition.includes(this.query)) {
+                        } else if (entry.definition.includes(this.query)) {
                             match = true;
                         }
                     }
-                    if (this.words[key].examples && (this.searchBy.exampleEn || this.searchBy.exampleLl)) {
-                        this.words[key].examples.forEach((example) => {
-                            if (this.searchBy.exampleLl && example[0].toLowerCase().includes(this.query)) {
+                    if (entry.examples && (this.searchBy.exampleEn || this.searchBy.exampleLl)) {
+                        entry.examples.forEach((example) => {
+                            if (this.searchBy.exampleLl && example.ll.toLowerCase().includes(this.query)) {
                                 match = true;
-                            } else if (this.searchBy.exampleEn && example[1].toLowerCase().includes(this.query)) {
+                            } else if (this.searchBy.exampleEn && example.en.toLowerCase().includes(this.query)) {
                                 match = true;
                             }
                         });
                     }
                     if (match) {
                         obj.push({
-                            key: key,
+                            entry: entry,
                             whole
                         });
                     }
@@ -106,15 +106,15 @@ class OioaLioa {
                     return a.whole ? -1 : 1;
                 });
         } else {
-            this.subset = Object.keys(this.words).reduce((obj, key) => {
+            this.subset = this.words.reduce((obj, entry) => {
                 obj.push({
-                    key: key,
+                    entry: entry,
                     whole: true
                 });
                 return obj;
             }, []);
         }
-        this.subsetLength = Object.keys(this.subset).length;
+        this.subsetLength = this.subset.length;
         document.title = `oioa lioa (${this.subsetLength})`;
         this.render();
     }
@@ -123,18 +123,18 @@ class OioaLioa {
         this.list.innerHTML = "";
         this.subset.forEach((set) => {
             let item = document.createElement("li");
-            let word = this.words[set.key];
+            let word = set.entry;
             let regexp = new RegExp(`(${this.query})`, "i");
             let definition = word.definition.replace(regexp, `<span class="regex-match">$1</span>`);
             item.setAttribute("class", set.whole ? "whole" : "partial");
-            item.innerHTML = `<h2>${set.key}</h2><p>${definition}</p>`;
+            item.innerHTML = `<h2>${word.head}</h2><p>${definition}</p>`;
             if (word.examples) {
                 let examples = document.createElement("ul");
                 examples.setAttribute("class", "examples");
                 word.examples.forEach((example) => {
                     let exampleItem = document.createElement("li");
-                    let exampleLl = example[0].replace(regexp, `<span class="regex-match">$1</span>`);
-                    let exampleEn = example[1].replace(regexp, `<span class="regex-match">$1</span>`);
+                    let exampleLl = example.ll.replace(regexp, `<span class="regex-match">$1</span>`);
+                    let exampleEn = example.en.replace(regexp, `<span class="regex-match">$1</span>`);
                     exampleItem.innerHTML = `<span class="example-ll">${exampleLl}</span><span class="example-en">${exampleEn}</span>`;
                     examples.appendChild(exampleItem);
                 });
